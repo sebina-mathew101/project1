42ba0b54a248e06a436706a690d50919dffb5274
sebina_mathew <sebinamathew0367@gmail.com>
2022-11-23 16:22:10 +0530

    addeded

---
+++ ecommerceproject/shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path,include
+
+from . import views
+app_name='shop'
+urlpatterns = [
+    path('',views.allProCat,name='allProCat'),
+    path('<slug:c_slug>/',views.allProCat,name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='prodCatdetail'),
+]
